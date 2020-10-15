403fe1be536a56a3353229df29bbb8c35dc91517
Francis Peckover <FrancisPeckover@users.noreply.github.com>
2020-10-15 12:20:32 +1000

    try

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php 
+    // If you are using Composer
     require 'vendor/autoload.php';
 
-    $from = new SendGrid\Email(null, "test@example.com");
-    $subject = "Hello World from the SendGrid PHP Library!";
-    $to = new SendGrid\Email(null, "test@example.com");
-    $content = new SendGrid\Content("text/plain", "Hello, Email!");
-    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+    $request_body = json_decode('{
+    "personalizations": [
+        {
+        "to": [
+            {
+            "email": "test@example.com"
+            }
+        ],
+        "subject": "Hello World from the SendGrid PHP Library!"
+        }
+    ],
+    "from": {
+        "email": "test@example.com"
+    },
+    "content": [
+        {
+        "type": "text/plain",
+        "value": "Hello, Email!"
+        }
+    ]
+    }');
 
     $apiKey = getenv('SENDGRID_API_KEY');
     $sg = new \SendGrid($apiKey);
 
-    $response = $sg->client->mail()->send()->post($mail);
+    $response = $sg->client->mail()->send()->post($request_body);
     echo $response->statusCode();
-    echo $response->headers();
     echo $response->body();
+    echo $response->headers();
 ?>
 
 <html lang="en">
